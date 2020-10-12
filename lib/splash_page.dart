76c35c7af25d56fed8cc641b5a3c13f1fcef7dd9
SONGHONGYANG419 <SONGHONGYANG419@pingan.com.cn>
2020-10-12 21:06:32 +0800

    NEW:Splash页面增加倒计时进入主页

--- lib/splash_page.dart
+++ lib/splash_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_im/common/touch_callback.dart';
 import 'package:flutter_im/router/page_id.dart';
 import 'package:flutter_im/utils/file_util.dart';
+import 'package:flutter_im/utils/timer_util.dart';
 
 class SplashPage extends StatefulWidget {
   @override
@@ -9,12 +11,41 @@ class SplashPage extends StatefulWidget {
 }
 
 class LoadingState extends State<SplashPage> {
+
+  TimerUtil _timerUtil;
+
+  int _count = 3;
+
   @override
   void initState() {
     super.initState();
-    Future.delayed(Duration(seconds: 3), () {
-      Navigator.of(context).pushReplacementNamed(PageId.GROUP_MAIN);
+    _initSplash();
+  }
+
+  @override
+  void dispose() {
+    if (null != _timerUtil) {
+      _timerUtil.cancel();
+    }
+    super.dispose();
+  }
+
+  _initSplash() {
+    _timerUtil = new TimerUtil(mTotalTime: 3 * 1000);
+    _timerUtil.setOnTimerTickCallback((int tick) {
+      double _tick = tick / 1000;
+      setState(() {
+        _count = _tick.toInt();
+      });
+      if (_tick == 0) {
+        _goNext();
+      }
     });
+    _timerUtil.startCountDown();
+  }
+
+  _goNext() {
+    Navigator.of(context).pushReplacementNamed(PageId.GROUP_MAIN);
   }
 
   @override
@@ -27,6 +58,33 @@ class LoadingState extends State<SplashPage> {
             fit: BoxFit.cover,
             width: double.infinity,
             height: double.infinity,
+          ),
+          Positioned(
+            bottom: 20,
+            right: 20,
+            child: Container(
+              padding: EdgeInsets.only(left: 24, right: 24, top: 12, bottom: 12),
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.all(Radius.circular(4)),
+              ),
+              child: TouchCallBack(
+                normalColor: Colors.transparent,
+                pressedColor: Colors.transparent,
+                child: Text(
+                  "跳过$_count",
+                  style: TextStyle(
+                    decoration: TextDecoration.none,
+                    fontSize: 14,
+                    color: Colors.white,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+                callBack: () {
+                  _goNext();
+                },
+              ),
+            ),
           ),
         ],
       ),
