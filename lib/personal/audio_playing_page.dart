ddad95d7ba84bfb86e1a7435cb35b18ea37ad242
SONGHONGYANG419 <SONGHONGYANG419@pingan.com.cn>
2020-10-16 09:15:54 +0800

    NEW:支持本地视频播放

--- lib/personal/audio_playing_page.dart
+++ lib/personal/audio_playing_page.dart
@@ -1,144 +1,93 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_im/utils/im_tools.dart';
 import 'package:video_player/video_player.dart';
-
+/*
+* 视频播放
+*/
 class AudioPlayingPage extends StatefulWidget {
   @override
-  State createState() => _ButterFlyAssetVideoState();
+  State<StatefulWidget> createState() {
+    return VideoPlayerState();
+  }
 }
 
-class _ButterFlyAssetVideoState extends State<AudioPlayingPage> {
+class VideoPlayerState extends State<AudioPlayingPage> {
   VideoPlayerController _controller;
-
+  VoidCallback _listener;
   String _url;
 
   String _title;
 
-  @override
-  void initState() {
-    super.initState();
-    _controller = VideoPlayerController.asset('assets/video/a.mp4');
-
-    _controller.addListener(() {
-      setState(() {});
-    });
-    _controller.setLooping(true);
-    _controller.initialize().then((_) => setState(() {}));
-    _controller.play();
-  }
+  bool _isFirst = true;
 
   @override
   void dispose() {
-    _controller.dispose();
     super.dispose();
+    _controller.removeListener(_listener);
+    _controller.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    if (_isFirst) {
+      _isFirst = false;
+      List<String> arguments = ModalRoute.of(context).settings.arguments;
+      assert(arguments != null && arguments.length == 2, "视频播放页面参数错误");
+      _title = arguments[0];
+      _url = arguments[1];
+      if (IMUtils.isStringNotEmpty(_url)) {
+        if (_url.startsWith('http')) {
+          _controller = VideoPlayerController.network(_url)
+            ..initialize().then((_) {
+              setState(() {
+                _controller.addListener(_listener);
+                _controller.play();
+              });
+            });
+        } else if (_url.contains('assets/video/')) {
+          _controller = VideoPlayerController.asset(_url)
+            ..initialize().then((_) {
+              setState(() {
+                _controller.addListener(_listener);
+                _controller.play();
+              });
+            });
+        }
+      }
+    }
 
-    List<String> arguments = ModalRoute.of(context).settings.arguments;
-    assert(arguments != null && arguments.length == 2, "视频播放页面参数错误");
-    _title = arguments[0];
-    _url = arguments[1];
-
-    return SingleChildScrollView(
-      child: Column(
-        children: <Widget>[
-          Container(
-            padding: const EdgeInsets.only(top: 20.0),
-          ),
-          const Text('With assets mp4'),
-          Container(
-            padding: const EdgeInsets.all(20),
-            child: AspectRatio(
-              aspectRatio: _controller.value.aspectRatio,
-              child: Stack(
-                alignment: Alignment.bottomCenter,
-                children: <Widget>[
-                  VideoPlayer(_controller),
-                  _ControlsOverlay(controller: _controller),
-                  VideoProgressIndicator(_controller, allowScrubbing: true),
-                ],
-              ),
+    double ratio;
+    if (_controller.value.initialized) {
+      double audioWidth = _controller.value.size.width;
+      double audioHeight = _controller.value.size.height;
+      ratio = audioWidth / audioHeight;
+    }
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.white,
             ),
-          ),
-        ],
+            onPressed: () {
+              Navigator.pop(context);
+            }),
       ),
-    );
-  }
-}
-
-class _ControlsOverlay extends StatelessWidget {
-  const _ControlsOverlay({Key key, this.controller}) : super(key: key);
-
-  static const _examplePlaybackRates = [
-    0.25,
-    0.5,
-    1.0,
-    1.5,
-    2.0,
-    3.0,
-    5.0,
-    10.0,
-  ];
-
-  final VideoPlayerController controller;
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        AnimatedSwitcher(
-          duration: Duration(milliseconds: 50),
-          reverseDuration: Duration(milliseconds: 200),
-          child: controller.value.isPlaying
-              ? SizedBox.shrink()
-              : Container(
-            color: Colors.black26,
-            child: Center(
-              child: Icon(
-                Icons.play_arrow,
-                color: Colors.white,
-                size: 100.0,
+      body: _controller.value.initialized
+          ? Center(
+              child: Container(
+                width: double.infinity,
+                height: MediaQuery
+                    .of(context)
+                    .size
+                    .width / ratio,
+                child: VideoPlayer(_controller),
               ),
-            ),
-          ),
-        ),
-        GestureDetector(
-          onTap: () {
-            controller.value.isPlaying ? controller.pause() : controller.play();
-          },
-        ),
-        Align(
-          alignment: Alignment.topRight,
-          child: PopupMenuButton<double>(
-            initialValue: controller.value.playbackSpeed,
-            tooltip: 'Playback speed',
-            onSelected: (speed) {
-              controller.setPlaybackSpeed(speed);
-            },
-            itemBuilder: (context) {
-              return [
-                for (final speed in _examplePlaybackRates)
-                  PopupMenuItem(
-                    value: speed,
-                    child: Text('${speed}x'),
-                  )
-              ];
-            },
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                // Using less vertical padding as the text is also longer
-                // horizontally, so it feels like it would need more spacing
-                // horizontally (matching the aspect ratio of the video).
-                vertical: 12,
-                horizontal: 16,
-              ),
-              child: Text('${controller.value.playbackSpeed}x'),
-            ),
-          ),
-        ),
-      ],
+            )
+          : Container(),
     );
   }
 }
